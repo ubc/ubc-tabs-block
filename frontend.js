6f1cff013fce18e5c06547dc5fe39b82960a6c29
Kelvin <kelvin@longevitygraphics.com>
2020-03-30 11:44:23 -0700

    fix linting errors in frontend.js

--- frontend.js
+++ frontend.js
@@ -5,21 +5,18 @@
 import './src/blocks/tabs/style.scss';
 import 'a11y-accordion-tabs';
 
-if(window.location.hash) {
-    var hash = window.location.hash.substring(1);
-    console.log(hash);
-    var element = document.getElementById(hash);
-    if(element){
-        let count = 0;
-        while( (element = element.previousSibling ) != null) {
-            count++;
-        }
-        console.log(count);
-        const tabs = document.getElementsByClassName('wp-block-ubc-tabs');
-        console.log(tabs[0].dataset.selectedTab);
-        tabs[0].dataset.selectedTab = count;
-        console.log(tabs[0].dataset.selectedTab);
-    }
+if ( window.location.hash ) {
+	const hash = window.location.hash.substring( 1 );
+	let element = document.getElementById( hash );
+	if ( element ) {
+		let count = 0;
+		while ( ( element = element.previousSibling ) !== null ) {
+			count++;
+		}
+		const tabs = document.getElementsByClassName( 'wp-block-ubc-tabs' );
+		tabs[ 0 ].dataset.selectedTab = count;
+	}
 }
-var tabs = document.querySelectorAll('.wp-block-ubc-tabs');
-tabs.forEach(tab => new AccordionTabs(tab))
+const tabs = document.querySelectorAll( '.wp-block-ubc-tabs' );
+// eslint-disable-next-line no-undef
+tabs.forEach( ( tab ) => new AccordionTabs( tab ) );
